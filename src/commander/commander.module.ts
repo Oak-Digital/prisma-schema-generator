b890f61a32818d531b3b33da7b09bad1e63209d0
David Koch Gregersem <david.koch@oakdigital.dk>
2022-11-29 14:28:11 +0100

    Commander uses dawa parser

--- src/commander/commander.module.ts
+++ src/commander/commander.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DataParserModule } from 'src/data-parser/data-parser.module';
 import { FileWriterModule } from 'src/file-writer/file-writer.module';
 import { MockDataModule } from 'src/mock-data/mock-data.module';
 import { GenerateCommand } from './generate-command';
 
 @Module({
     providers: [GenerateCommand],
-    imports: [MockDataModule, FileWriterModule],
+    imports: [MockDataModule, FileWriterModule, DataParserModule],
 })
 export class CommanderModule {}
--- src/commander/generate-command.ts
+++ src/commander/generate-command.ts
@@ -1,29 +1,28 @@
 import { exec } from 'child_process';
-import { existsSync } from 'fs';
 import { Command, CommandRunner, Option } from 'nest-commander';
-import path from 'path';
+import { DawaParser } from 'src/data-parser/parsers/dawa-parser';
 import { FileWriter } from 'src/file-writer/file-writer';
 import { SchemaTester } from 'src/mock-data/schema-tester';
 
 @Command({ name: 'generate', description: '', options: { isDefault: true } })
 export class GenerateCommand extends CommandRunner {
-    constructor(private tester: SchemaTester, private writer: FileWriter) {
+    constructor(private dawa: DawaParser, private writer: FileWriter) {
         super();
     }
 
     async run(inputs: string[], options: Record<string, any>): Promise<void> {
-        const path = options.output ?? '.';
-
-        // console.log('path is', path);
-
+        const path = options.output ?? './.out/';
         const fileName = 'schema.prisma';
 
-        const fileContent = this.tester.testRender();
+        await this.dawa.build();
+        const fileContent = this.dawa.render();
 
         await this.writer.writeFile(path, fileName, fileContent);
 
+        const prismaBin = 'prisma';
+
         // Format resulting file
-        exec(`npx prisma format --schema ${path}`, (error) => {
+        exec(`${prismaBin} format --schema ${path}`, (error) => {
             if (error) console.error(`${error}`);
         });
     }
@@ -34,7 +33,7 @@ export class GenerateCommand extends CommandRunner {
         description: 'where the resulting schema should be written',
     })
     parsePathOption(option: string) {
-        // const pathArray = option.split('/'); // TODO: windows sepparators
+        // const pathArray = option.split('/'); // TODO: windows separators
 
         // TODO
 
